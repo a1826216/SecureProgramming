db8f97db1128f5c62627f532ef3183b252932b6a
a1826216 <a1826216@adelaide.edu.au>
2024-09-15 18:19:41 +0930

    added group member names to readme, test echo server using python websockets library

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ An implementation of the OLAF Neighbourhood protocol as defined here:
 
 https://github.com/xvk-64/2024-secure-programming-protocol
 
+## Group Members
+Alanna Anna Shibu
+Samuel Hunter
+Phapada Thanachotiwit
+Henry Winter
+
 ## Setup Instructions
 To be done
---
+++ client/test_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from websockets.sync.client import connect
+
+def hello():
+    with connect("ws://localhost:8765") as websocket:
+        websocket.send("Hello world!")
+        message = websocket.recv()
+        print(f"Received: {message}")
+
+        websocket.send("test message!")
+        message = websocket.recv()
+        print(f"Received: {message}")
+
+hello()
