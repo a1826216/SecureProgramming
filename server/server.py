4cec9e8a542862055abb1de6f05e6c829e65fac8
a1826216 <a1826216@adelaide.edu.au>
2024-09-26 15:09:37 +0930

    basic function for testing validity of client-sent messages

--- server/server.py
+++ server/server.py
@@ -22,14 +22,31 @@ class Server:
         self.neighbourhood_clients = {}
 
     # Check if a message is a valid OLAF Neighbourhood protocol message
-    def check_msg_is_valid(self, message):
-        # Check if a message has the required JSON headers
-        return False
+    def check_msg_is_valid(self, message:dict):
+        # List of valid keys for messages sent by client
+        valid_keys = ["type","data","counter","signature"]
+
+        # List of valid data types for messages sent by client
+        valid_data_types = ["hello", "chat", "public_chat"]
+
+        # Check if message has the required keys (and nothing else)
+        for key in message.keys():
+            if key not in valid_keys:
+                return False
+            
+        # Check validity of data type (content is not checked at the moment)
+        data_type = message["data"]["type"]
+        if data_type not in valid_data_types:
+            return False
+            
+        return True
     
     # Echo message back to client (for testing)
     async def echo(self, websocket):
         async for message in websocket:
             print(message)
+            # print(json.loads(message).keys())
+            print(self.check_msg_is_valid(json.loads(message)))
             await websocket.send(message)
 
     # Run server
