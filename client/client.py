bd45d4bf1a6cdc36d1d24f1db98bde527c5aa9a9
Phapada Thanachotiwit <101790110+hililyn@users.noreply.github.com>
2024-10-01 23:31:44 +0930

    Update client.py for aes

--- client/client.py
+++ client/client.py
@@ -9,12 +9,13 @@ from aioconsole import ainput
 from Crypto.Signature import pss
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 class Client:
     def __init__(self, uri):
         # Address of server to connect to
         self.uri = uri
-        
+
         # Counter value (to be sent with signed data)
         self.counter = 0
 
@@ -25,125 +26,172 @@ class Client:
         self.key_pair = RSA.generate(2048)
 
         # Exported PEM of public key
-        self.public_key = self.key_pair.public_key().export_key()
+        self.public_key = self.key_pair.public_key().export_key().decode('utf-8')
 
-        # Exported PEM of private key
-        self.private_key = self.key_pair.export_key()
-
-        # Client's own client ID
-        self.client_id = SHA256.new(base64.b64encode(bytes(self.public_key.decode('utf-8'), 'utf-8'))).hexdigest()
+        # Client's own client ID (SHA256 of base64 encoded public key)
+        self.client_id = hashlib.sha256(base64.b64encode(self.public_key.encode('utf-8'))).hexdigest()
 
         # List of clients on home server (excluding this one)
         self.clients = {}
 
-        # List of clients on other servers
-        self.neighbourhood_clients = {}
-
-    # Helper function to generate signed data messages
-    def generate_signed_data(self, data:dict):
+    # Function to generate signed data messages
+    def generate_signed_data(self, data):
         # Concatenate data and counter (this will form the signature)
-        data_c = bytes(json.dumps(data)+str(self.counter), 'utf-8')
-        
-        # Get SHA256 sum of data and counter
-        hash = SHA256.new(data_c)
+        data_c = bytes(json.dumps(data) + str(self.counter), 'utf-8')
 
-        # Sign signature with RSA private key
-        signature = pss.new(self.key_pair, salt_bytes=32).sign(hash)
+        # Get SHA256 sum of data and counter
+        hash_value = SHA256.new(data_c)
+
+        # Sign the hash using the RSA private key
+        signature = pss.new(self.key_pair).sign(hash_value)
 
         # Base64 encode the signature
-        signature = base64.b64encode(signature)
-        
+        signature_b64 = base64.b64encode(signature).decode('utf-8')
+
+        # Create the signed data structure
         signed_data = {
             "type": "signed_data",
             "data": data,
             "counter": self.counter,
-            "signature": bytes.decode(signature)
+            "signature": signature_b64
         }
 
         # Increment counter value (for future signed data messages)
         self.counter += 1
 
         return json.dumps(signed_data)
-    
-    # Helper function to return client ID (for adding clients to server client list)
-    def get_client_id(self, public_key):
-        return SHA256.new(base64.b64encode(bytes(public_key, 'utf-8'))).hexdigest()
 
     # Helper function to generate and send a hello message
     async def send_hello(self):
-        # Generate hello message
-        data = {
-            "type": "hello", 
-            "public_key": self.public_key.decode('utf-8')
-        }
+        # Create hello message with the client's public key
+        data = {"type": "hello", "public_key": self.public_key}
 
+        # Generate signed data for the hello message
         hello_msg = self.generate_signed_data(data)
 
-        # Send hello message
+        # Send the hello message
         await self.websocket.send(hello_msg)
 
-    # Helper function to generate and send a public chat message
+    # Function to encrypt a message using AES-GCM
+    def aes_encrypt_message(self, aes_key, message):
+        aes_cipher = AES.new(aes_key, AES.MODE_GCM)
+        ciphertext, tag = aes_cipher.encrypt_and_digest(message.encode('utf-8'))
+
+        # Return the encrypted message components in base64 format for safe transmission
+        return {
+            "ciphertext": base64.b64encode(ciphertext).decode('utf-8'),
+            "nonce": base64.b64encode(aes_cipher.nonce).decode('utf-8'),
+            "tag": base64.b64encode(tag).decode('utf-8')
+        }
+
+    # Function to decrypt a message using AES-GCM
+    def aes_decrypt_message(self, aes_key, encrypted_message):
+        ciphertext = base64.b64decode(encrypted_message["ciphertext"])
+        nonce = base64.b64decode(encrypted_message["nonce"])
+        tag = base64.b64decode(encrypted_message["tag"])
+
+        aes_cipher = AES.new(aes_key, AES.MODE_GCM, nonce=nonce)
+        try:
+            # Decrypt the message and verify its integrity
+            message = aes_cipher.decrypt_and_verify(ciphertext, tag)
+            return message.decode('utf-8')
+        except ValueError:
+            print("Decryption failed. Invalid AES key or message tampered.")
+            return None
+
+    # Function to encrypt the AES key using the recipient's RSA public key
+    def encrypt_aes_key(self, recipient_public_key):
+        # Generate a 256-bit AES key
+        aes_key = secrets.token_bytes(32)
+
+        # Import the recipient's RSA public key and encrypt the AES key
+        recipient_key = RSA.import_key(recipient_public_key.encode('utf-8'))
+        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+        encrypted_aes_key = cipher_rsa.encrypt(aes_key)
+
+        return aes_key, base64.b64encode(encrypted_aes_key).decode('utf-8')
+
+    # Function to decrypt the AES key using the client's own RSA private key
+    def decrypt_aes_key(self, encrypted_aes_key):
+        encrypted_aes_key_bytes = base64.b64decode(encrypted_aes_key)
+
+        # Decrypt the AES key using the client's private RSA key
+        cipher_rsa = PKCS1_OAEP.new(self.key_pair)
+        aes_key = cipher_rsa.decrypt(encrypted_aes_key_bytes)
+
+        return aes_key
+
+    # Function to send a public chat message to all connected clients
     async def send_public_chat(self, message):
-        # Generate public chat message
+        # Create the public chat message structure
         data = {
             "type": "public_chat",
             "sender": self.client_id,
             "message": message
         }
 
+        # Generate signed data for the public chat message
         public_chat_msg = self.generate_signed_data(data)
 
-        # Send public chat message
-        print("Sending public chat message...")
+        # Send the public chat message to the server
         await self.websocket.send(public_chat_msg)
+        print("Public chat message sent.")
 
-    # Generate and send an encrypted chat message
-    async def send_chat(self, message, recipients):
-        # Split recipients list by space
-        client_list = recipients.split(" ")
+    # Function to send an encrypted private chat message
+    async def send_chat(self, message, recipient_id):
+        if recipient_id not in self.clients:
+            print(f"Client {recipient_id} not found in client list.")
+            return
 
-        # Chat message (to be encrypted using AES)
-        chat = {
-            "participants": [self.client_id] + client_list,
-            "message": message
+        # Get the recipient's public key
+        recipient_public_key = self.clients[recipient_id]['public_key']
+
+        # Encrypt the AES key with the recipient's RSA public key
+        aes_key, encrypted_aes_key = self.encrypt_aes_key(recipient_public_key)
+
+        # Encrypt the message using AES
+        encrypted_message = self.aes_encrypt_message(aes_key, message)
+
+        # Create the chat message structure
+        data = {
+            "type": "chat",
+            "sender": self.client_id,
+            "recipient": recipient_id,
+            "aes_key": encrypted_aes_key,
+            "message": encrypted_message
         }
 
-        # Generate 16-byte initialisation vector
-        iv = secrets.token_bytes(16)
+        # Generate signed data and send it
+        chat_msg = self.generate_signed_data(data)
+        await self.websocket.send(chat_msg)
+        print("Encrypted chat message sent.")
 
-        
-
-
-    # Send client list request
+    # Function to send a client list request
     async def client_list_request(self):
-        # Send message
+        # Create a client list request message
         message = {"type": "client_list_request"}
         await self.websocket.send(json.dumps(message))
 
-    # Handle client list
+    # Handle the received client list from the server
     async def handle_client_list(self, message):
-        # Create list of client IDs
+        # Create a temporary list to store the received clients
         temp_list = {}
-        
-        # Handle clients on all servers
+
         for server in message["servers"]:
             for public_key in server['clients']:
-                client_id = self.get_client_id(public_key)
-                    
-                # Add client if client ID is not this client
+                client_id = hashlib.sha256(base64.b64encode(public_key.encode('utf-8'))).hexdigest()
                 if client_id != self.client_id:
                     temp_list[client_id] = {
-                        "home_server": server["address"], 
+                        "home_server": server["address"],
                         "fingerprint": client_id,
                         "public_key": public_key,
                     }
 
-        # Replace client list
+        # Replace the client's stored client list with the received list
         self.clients = temp_list
 
-    # Print client list
+    # Print the list of clients
     def print_client_list(self):
-        # List clients on all servers
         print("List of clients:")
         for client in self.clients:
             home_server = self.clients[client]["home_server"]
@@ -159,17 +207,23 @@ class Client:
                 chat = message["data"]["message"]
                 print(f"From {sender} (public): {chat}")
             case "chat":
-                print("regular chat")
+                sender = message["data"]["sender"]
+                encrypted_message = message["data"]["message"]
+                encrypted_aes_key = message["data"]["aes_key"]
+
+                # Decrypt the AES key using the client's private key
+                aes_key = self.decrypt_aes_key(encrypted_aes_key)
+
+                # Decrypt the message using the AES key
+                decrypted_message = self.aes_decrypt_message(aes_key, encrypted_message)
+
+                if decrypted_message:
+                    print(f"From {sender} (private): {decrypted_message}")
             case _:
-                print("Invalid message type sent to client")
+                print("Invalid message type received")
 
-    # Handle chat sent by another client
-    async def handle_chat(self):
-        pass
-
-    # Listen for messages
+    # Listen for messages from the server
     async def listen(self):
-        # Receive message
         async for message in self.websocket:
             data = json.loads(message)
             if "type" not in data.keys():
@@ -177,7 +231,7 @@ class Client:
             else:
                 await self.handle_messages(data)
 
-    # Handle incoming messages
+    # Handle the received message based on its type
     async def handle_messages(self, message):
         message_type = message["type"]
 
@@ -189,22 +243,21 @@ class Client:
             case _:
                 print("Invalid message type received")
 
+    # Run the client
     async def run(self):
-        print("Starting OLAF Neighbourhood client...")
         print(f"Connecting to server at {self.uri}...")
         self.websocket = await websockets.connect(self.uri)
 
         # Send hello message
-        print("Sending hello message...")
         await self.send_hello()
 
-        # Listen for incoming messages
+        # Start listening for incoming messages
         asyncio.ensure_future(self.listen())
 
-        # Get client list
+        # Request the client list
         await self.client_list_request()
 
-        # Main loop
+        # Main loop to handle user inputs
         while True:
             prompt = await ainput("> ")
 
@@ -214,9 +267,10 @@ class Client:
                     await self.send_public_chat(public_message)
                 case "chat":
                     await self.client_list_request()
-                    client_id_list = await ainput("Enter list of Client ID's (separated by spaces): ")
+                    self.print_client_list()
+                    recipient_id = await ainput("Enter recipient Client ID: ")
                     chat_message = await ainput("Enter a message: ")
-                    await self.send_chat(chat_message, client_id_list)
+                    await self.send_chat(chat_message, recipient_id)
                 case "list":
                     await self.client_list_request()
                     self.print_client_list()
@@ -230,27 +284,4 @@ class Client:
 
 if __name__ == "__main__":
     client = Client("ws://localhost:8765")
-
-    # Run client
     asyncio.run(client.run())
-
-
-
-    # # Basic tests for client functionality
-    # async def tests(self):
-    #     async with websockets.connect(self.uri) as websocket:
-    #         # Try to send a public chat before hello is sent
-    #         await self.send_public_chat(websocket, "public chat!")
-            
-    #         # Send hello and get client list
-    #         await self.send_hello(websocket)
-    #         await self.client_list_request(websocket)
-
-    #         # Send a duplicate hello
-    #         await self.send_hello(websocket)
-
-    #         # Send a public chat
-    #         await self.send_public_chat(websocket, "public chat!")
-
-    #         # await websocket.close()
-    #         return
